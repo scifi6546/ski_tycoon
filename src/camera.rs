d9579976172a0249c90372c7a2c74b81fc4adc31
Nicholas Alexeev <nickalexeev@protonmail.com>
2020-12-17 10:06:10 -0900

    fixed build errors

--- src/camera.rs
+++ src/camera.rs
@@ -1,7 +1,5 @@
-use super::log;
-use nalgebra::{Matrix4, Perspective3, Point, Vector3};
+use nalgebra::{Matrix4,  Point, Vector3};
 pub struct Camera {
-    matrix: Perspective3<f32>,
     /// Origin about which camera rotates
     origin: Vector3<f32>,
     /// how far away the camera is from its origin
@@ -13,7 +11,6 @@ pub struct Camera {
 impl Camera {
     pub fn new(origin: Vector3<f32>, radius: f32, phi: f32, theta: f32) -> Self {
         Self {
-            matrix: Perspective3::new(1.0, 3.14 / 4.0, 0.1, 1000.0),
             origin,
             radius,
             phi,
